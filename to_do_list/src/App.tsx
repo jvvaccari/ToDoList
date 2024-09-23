7369655b0190a45edcd7deed222ea964c8f35927
joao <joao.vaccari@majovdev.com.br>
2024-09-22 23:40:07 -0300

    Making the form structure

--- to_do_list/src/App.tsx
+++ to_do_list/src/App.tsx
@@ -1,14 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
 import Header from './components/header';
 import Footer from './components/footer';
-import ContentBox from './components/inputContent'
-
+import TaskForm from './components/taskForm';
+import OutputContent from './components/outputContent';
+// CSS
+import styles from './App.module.css';
+// Interface
+import { ITask } from './interfaces/ITask';
 
 function App() {
+
+  const [taskList, setTaskList] = useState<ITask[]>([]);
+
   return (
     <div>
       <Header />
-      <ContentBox />
+      <main className={styles.main}>
+        <h2>What do you do?</h2>
+        <TaskForm btnInput='Create Task' taskList={taskList} setTaskList={setTaskList}/>
+        <h3>Your tasks:</h3>
+        <OutputContent />
+      </main>
       <Footer />
     </div>
   );
---
+++ to_do_list/src/components/taskForm.tsx
@@ -0,0 +1,83 @@
+import React, {useState,ChangeEvent,FormEvent,useEffect} from 'react';
+import Button from '@mui/material/Button';
+import TextField from '@mui/material/TextField';
+import styles from './InputContent.module.css';
+
+// Interface
+import { ITask } from '../interfaces/ITask';
+
+interface ITaskProps{
+    btnInput: string;
+    taskList: ITask[];
+    setTaskList?: React.Dispatch<React.SetStateAction<ITask[]>>;
+}
+
+const TaskForm = ({btnInput,taskList,setTaskList}: ITaskProps) => {
+
+    // const [id,setId] = useState<string>("");
+    const [title, setTitle] = useState<string>("");
+    const [difficulty, setDifficulty] = useState<string>("");
+
+    const addTaskHandler = (e: FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        const id = Math.floor(Math.random() * 1000);
+        const newTask:ITask = {id,title,difficulty};
+
+        setTaskList!([...taskList, newTask]);
+
+        setTitle("");
+        setDifficulty("");
+
+        console.log(taskList);
+    };
+
+    const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+        const {name, value} = e.target;
+        
+        if(name === "title"){
+            setTitle(value);
+        }else if(name === "difficulty"){
+            setDifficulty(value);
+        }
+    }
+
+    return (
+        <form onSubmit={addTaskHandler}>
+            <div className={styles.input_content}>
+                
+                <TextField 
+                    name="title"
+                    id="outlined-basic" 
+                    label="Task title" 
+                    variant="outlined" 
+                    size="small" 
+                    sx={{width: '320px'}}
+                    onChange={handleChange}
+                    value= {title}
+                />
+                
+                <TextField 
+                    name="difficulty"
+                    id="outlined-basic" 
+                    label="Task difficulty" 
+                    variant="outlined" 
+                    size="small" 
+                    sx={{width: '320px'}}
+                    onChange={handleChange}
+                    value= {difficulty}
+                />
+              
+                <Button 
+                    type="submit" 
+                    value={btnInput} 
+                    variant="contained" 
+                    sx={{width: '320px',backgroundColor: '#61dafb',color: 'black'}}
+                >
+                    {btnInput}
+                </Button>
+            </div> 
+        </form>   
+    );
+}
+
+export default TaskForm;
